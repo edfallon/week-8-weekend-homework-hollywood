94393b3ffc2dadcfca8afd48adfc0d1dcf37285c
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-18 18:25:00 +0100

    created abstract class Person with getters and setters

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,4 +1,48 @@
 package models;
 
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+
 public abstract class Person {
+
+    private int id;
+    private String name;
+    private double money;
+
+
+    public Person() {
+    }
+
+    public Person(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
 }
